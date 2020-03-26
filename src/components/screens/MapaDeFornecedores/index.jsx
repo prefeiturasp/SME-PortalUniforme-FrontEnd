86c2f2ec4ec8ebc4a94faf21065dff94803dabd9
Calvin Rossinhole <calvin.masters@gmail.com>
2020-03-26 10:47:45 -0300

    paginacao nas lojas

--- src/components/screens/MapaDeFornecedores/index.jsx
+++ src/components/screens/MapaDeFornecedores/index.jsx
@@ -1,16 +1,19 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import Mapa from "components/Mapa";
 import "./style.scss";
 import { getLojasCredenciadas } from "services/uniformes.service";
 import Select from "components/Select";
 import { ORDENAR_OPCOES } from "./constants";
 import { LoadingCircle } from "components/LoadingCircle";
+import { QUANTIDADE_POR_PAGINA } from "components/Paginacao/constants";
+import { Paginacao } from "components/Paginacao";
 
 export class MapaDeFornecedores extends Component {
   constructor() {
     super();
     this.state = {
-      lojas: null
+      lojas: null,
+      pagina: 1
     };
   }
 
@@ -28,16 +31,16 @@ export class MapaDeFornecedores extends Component {
       tipoUniformeSelecionados,
       endereco
     } = this.props.location.state;
-    const { lojas } = this.state;
+    const { lojas, pagina } = this.state;
     return (
       <div>
         <div
-          className={`w-100 sociedade-governo text-white mt-5 ${!lojas &&
+          className={`w-100 sociedade-governo mt-5 ${!lojas &&
             "opaco"}`}
         >
           <div className="container">
             <div className="col-lg-12 mb-lg-0">
-              <h3>Lojas mais próximas</h3>
+              <h3 className="text-white">Lojas mais próximas</h3>
             </div>
           </div>
         </div>
@@ -64,20 +67,29 @@ export class MapaDeFornecedores extends Component {
                     <div className="col-5">Itens de uniforme disponíveis</div>
                   </div>
                   {lojas &&
-                    lojas.map((loja, key) => {
-                      return (
-                        <div key={key} className="loja-collapse">
-                          <div className="row">
-                            <div className="col-7 font-weight-bold">
-                              {loja.nome_fantasia.toUpperCase()}
-                              <div className="clique-mensagem">
-                                Clique no + para dados de contato e preço
+                    lojas
+                      .slice(
+                        QUANTIDADE_POR_PAGINA * (pagina - 1),
+                        QUANTIDADE_POR_PAGINA * pagina
+                      )
+                      .map((loja, key) => {
+                        return (
+                          <div key={key} className="loja-collapse">
+                            <div className="row">
+                              <div className="col-7 font-weight-bold">
+                                {loja.nome_fantasia.toUpperCase()}
+                                <div className="clique-mensagem">
+                                  Clique no + para dados de contato e preço
+                                </div>
                               </div>
                             </div>
                           </div>
-                        </div>
-                      );
-                    })}
+                        );
+                      })}
+                  {lojas && <Paginacao
+                    onChange={pagina => this.setState({ pagina })}
+                    total={lojas.length}
+                  />}
                 </div>
               </div>
               <div className="col-lg-6 col-sm-12 mapa-completo">
