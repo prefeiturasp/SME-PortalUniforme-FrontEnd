3d71352d2db88103b2403c899e9df5473d87a934
Kelwy <kelwyduarte@gmail.com>
2021-01-27 14:20:04 -0300

    Teste modificando entrepoint variavel ambiente

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,9 +16,13 @@ set -xe
 set -xe
   : "${SENTRY_URL?Precisa de uma variavel de ambiente SENTRY_URL}"
 
+set -xe
+  : "${REACT_APP_ATUALIZACAO_CADASTRAL_URL?Precisa de uma variavel de ambiente REACT_APP_ATUALIZACAO_CADASTRAL_URL}"
+
 
 sed -i "s,API_URL_REPLACE_ME,$API_URL,g" /usr/share/nginx/html/static/js/main*.js
 sed -i "s,SENTRY_URL_REPLACE_ME,$SENTRY_URL,g" /usr/share/nginx/html/static/js/main*.js
 sed -i "s,SERVER_NAME,$SERVER_NAME,g" /etc/nginx/conf.d/default.conf
+sed -i "s,ATUALIZACAO_CADASTRAL_URL,$REACT_APP_ATUALIZACAO_CADASTRAL_URL,g" /etc/nginx/conf.d/default.conf
 
 exec "$@"
